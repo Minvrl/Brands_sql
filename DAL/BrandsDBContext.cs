658b50e8dbeb9e2a6e63d1acefd2978b2c3ddf2f
Minvrl <130864645+Minvrl@users.noreply.github.com>
2024-02-27 12:20:10 +0400

    Add files via upload

---
+++ DAL/BrandsDBContext.cs
@@ -0,0 +1,21 @@
+﻿using Brands_proj.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+
+namespace Brands_proj.DAL
+{
+    public class BrandsDBContext:DbContext
+    {
+        public DbSet<Brand> Brands { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("server=MIMI\\SQLEXPRESS;database=BrandsDB;trusted_connection=true");
+        }
+    }
+}
---
+++ Migrations/20240227081659_CRUDcreated.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Brands_proj.Migrations
+{
+    /// <inheritdoc />
+    public partial class CRUDcreated : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
